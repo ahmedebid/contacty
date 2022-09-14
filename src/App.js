499ccd28e7a818beec643c3677264d98465930d3
ahmedebid <ahmed-ebid@outlook.com>
2022-09-14 20:02:47 +0200

    Experimenting with this.setState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super(); // To invoke the constructor of the parent class (Component)
+
+    this.state = {
+      userInfo: {
+        name: "Ali",
+        age: 22,
+        education: "BSc",
+      },
+      company: "ZTM",
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hello, I'm {this.state.userInfo.name}, I'm {this.state.userInfo.age}{" "}
+            with {this.state.userInfo.education} in civil engineering and I work
+            in {this.state.company}
+          </p>
+          <button
+            onClick={() =>
+              this.setState(
+                () => {
+                  return {
+                    userInfo: {
+                      name: "Kamel",
+                      age: 12,
+                      education: "Secondary School",
+                    },
+                  };
+                },
+                () => console.log(this.state)
+              )
+            }
+          >
+            Change Name
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
